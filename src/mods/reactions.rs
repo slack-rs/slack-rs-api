7409e9eea0fe7041c7e710fa8a7c8f993db5db77
Benjamin Wasty <benny.wasty@gmail.com>
2017-05-12 16:36:33 +0200

    add ts to reactions::ListResponseItemMessage

--- src/mods/reactions.rs
+++ src/mods/reactions.rs
@@ -610,6 +610,7 @@ pub struct ListResponseItemFileComment {
 #[derive(Clone, Debug, Deserialize)]
 pub struct ListResponseItemMessage {
     pub channel: String,
+    pub ts: String,
     #[serde(rename = "type")]
     pub ty: String,
 }
