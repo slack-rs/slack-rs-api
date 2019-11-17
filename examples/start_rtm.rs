dc4627375c7681938c050d645a94bbf3191fce5a
David Hewson <dev@daveid.co.uk>
2019-11-17 22:05:33 +0000

    update existing examples

--- examples/start_rtm.rs
+++ examples/start_rtm.rs
@@ -1,11 +1,14 @@
-extern crate slack_api as slack;
+use slack_api as slack;
 
 use std::default::Default;
 use std::env;
 
-fn main() {
-    let token = env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN not set.");
-    let client = slack::default_client().unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // You can generate a legacy token to quickly test these apis
+    // https://api.slack.com/custom-integrations/legacy-tokens
+    let token = env::var("SLACK_API_TOKEN").map_err(|_| "SLACK_API_TOKEN env var must be set")?;
+    let client =
+        slack::default_client().map_err(|e| format!("Could not get default_client, {:?}", e))?;
 
     {
         let request = slack::rtm::StartRequest::default();
@@ -13,22 +16,25 @@ fn main() {
 
         if let Ok(response) = response {
             if let Some(channels) = response.channels {
-                let channels = channels.iter()
-                    .filter_map(|c| c.name.clone())
+                let channel_names = channels
+                    .iter()
+                    .filter_map(|c| c.name.as_ref())
                     .collect::<Vec<_>>();
 
-                println!("Got channels: {}", channels.join(", "));
+                println!("Got channels: {:?}", channel_names);
             }
 
             if let Some(users) = response.users {
-                let users = users.iter()
-                    .filter_map(|u| u.name.clone())
+                let user_names = users
+                    .iter()
+                    .filter_map(|u| u.name.as_ref())
                     .collect::<Vec<_>>();
 
-                println!("Got users: {}", users.join(", "));
+                println!("Got users: {:?}", user_names);
             }
         } else {
             println!("{:?}", response);
         }
     }
+    Ok(())
 }
