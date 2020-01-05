2abea17db628bfe1f2eb7e5fc545a7d8fee51eda
David Hewson <dev@daveid.co.uk>
2020-01-05 20:39:18 +0000

    store timestamp as microseconds to ease deriving Eq Ord and Hash

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -1,17 +1,25 @@
 use serde;
 
-#[derive(Debug, Clone, Copy, Default)]
-pub struct Timestamp(f64);
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct Timestamp(u64);
+
 impl From<f64> for Timestamp {
     fn from(t: f64) -> Self {
-        Timestamp(t)
+        let micro_seconds = t * 1_000_000.0;
+        Timestamp(micro_seconds as u64)
     }
 }
 
-impl std::ops::Deref for Timestamp {
-    type Target = f64;
-    fn deref(&self) -> &f64 {
-        &self.0
+impl Into<f64> for Timestamp {
+    fn into(self) -> f64 {
+        let seconds = (self.0 as f64) / 1_000_000.0;
+        seconds
+    }
+}
+impl Into<f64> for &Timestamp {
+    fn into(self) -> f64 {
+        let seconds = (self.0 as f64) / 1_000_000.0;
+        seconds
     }
 }
 
@@ -25,23 +33,24 @@ impl<'de> ::serde::Deserialize<'de> for Timestamp {
         let value = ::serde_json::Value::deserialize(deserializer)?;
         if let Some(s) = value.as_str() {
             s.parse::<f64>()
-            .map_err(|e| D::Error::custom(e))
-            .map(Into::into)
+                .map_err(|e| D::Error::custom(e))
+                .map(Into::into)
         } else if let Some(f) = value.as_f64() {
             Ok(f.into())
         } else if let Some(u) = value.as_u64() {
             Ok((u as f64).into())
         } else {
-            Err(D::Error::custom(
-                format!("expected a timestamp but got: {}", value.to_string()),
-            ))
+            Err(D::Error::custom(format!(
+                "expected a timestamp but got: {}",
+                value.to_string()
+            )))
         }
     }
 }
 
 impl Timestamp {
     pub fn to_param_value(&self) -> String {
-        let t: &f64 = self;
-        serde_json::to_string(t).unwrap()
+        let t: f64 = self.into();
+        serde_json::to_string(&t).unwrap()
     }
 }
